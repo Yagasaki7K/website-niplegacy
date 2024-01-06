850bf19c753608a69064e154c164426e98ba8810
Anderson Marlon <anderson18.marlon@gmail.com>
2024-01-06 14:00:15 -0300

    Fix: Links

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -20,7 +20,7 @@ const Prices = () => {
                         <h3>Vip Light (30 dias)</h3>
                         <p>Auto-loot / Área Vip / Painel Vip / 50% de EXP</p>
                         <p>Preço: R$ 15,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Vip%20Light" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -30,7 +30,7 @@ const Prices = () => {
                         <h3>Vip OhYeh (30 dias)</h3>
                         <p>Auto-loot / Área Oh Yeah / Painel Vip / 100% de EXP</p>
                         <p>Preço: R$ 40,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Vip%20OhYeh" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -40,7 +40,7 @@ const Prices = () => {
                         <h3>Personagens Especiais</h3>
                         <p>Madara, Itachi, Pain, Hashirama, Raikage e Sasori</p>
                         <p>Preço: R$20,00/cada ou R$50,00 Completo</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Personagem%20Especiais" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -50,7 +50,7 @@ const Prices = () => {
                         <h3>Personagens Supremo</h3>
                         <p>Básicos + Especiais + Tobi, Orochimaru, Konan, Darui, Hidan, Danzou, Tobirama, Yondaime, Mei, Killer Bee, Zabuza, Haku, Yugito</p>
                         <p>Preço: R$ 30,00/Cada ou R$100,00 Completo)</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Personagem%20Supremo" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -60,7 +60,7 @@ const Prices = () => {
                         <h3>Campo de Treino CT</h3>
                         <p>Campo de treinamento com logs de EXP (Permitido ficar AFK)</p>
                         <p>Preço: 1 Passe - R$20,00 / 2 Passes - R$35,00 / 3 Passes - R$60,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Passe%20de%20Campo%20de%20Treino%20CT" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -70,7 +70,7 @@ const Prices = () => {
                         <h3>Pack 1</h3>
                         <p>Vip Light (30 dias) + 20k de CASH</p>
                         <p>Preço: R$ 30,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Pack%201" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -80,7 +80,7 @@ const Prices = () => {
                         <h3>Pack 2</h3>
                         <p>Vip Light (30 dias) + 50k de Cash</p>
                         <p>Preço: R$ 40,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Pack%202" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -90,7 +90,7 @@ const Prices = () => {
                         <h3>Pack 3</h3>
                         <p>Vip Light (30 dias) + 100k de Cash + 10x Booster Event (1 hora a escolha)</p>
                         <p>Preço: R$ 50,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Pack%203" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -100,7 +100,7 @@ const Prices = () => {
                         <h3>Pack 4</h3>
                         <p>Vip Light (30 dias) ou Vip OhYeah (10 dias) + 125k + 10x Booster Event (1 hora a escolha)</p>
                         <p>Preço: R$ 75,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Pack%204" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -110,7 +110,7 @@ const Prices = () => {
                         <h3>Set Rikudou</h3>
                         <p>Protetor Rikudou (R$25) + Armadura Rikudou (R$25) + Capacete Rikudou (R$25)  + 15x Booster Event</p>
                         <p>Preço: R$ 75,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Set%20Rikudou" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
 
@@ -119,7 +119,7 @@ const Prices = () => {
                         <img src="/favicon.png" alt="" />
                         <h3>Booster Evento EXP</h3>
                         <p>10x = R$ 35,00 // 15x = R$ 70,00 <br /> 30x = R$ 100,00 // 50x = R$ 200,00</p>
-                        <a href="d">Comprar</a>
+                        <a href="https://wa.me/5519998018174?text=Eu%20estou%20interessado%20em%20adquirir%20o%20Boost%20de%20Evento%20EXP" target="_blank" rel="noreferrer">Comprar</a>
                     </div>
                 </div>
             </div>
